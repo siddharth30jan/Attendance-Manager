8ce772cce3ad793665e65a0297ed08babf075fc2
Siddharth Singh <siddharths51700@gmail.com>
2020-01-22 21:40:19 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose
 
 app.use("/api", require("./routes"));
 
+
+if(process.env.NODE_ENV=='production'){
+  app.use(express.static('client/build'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`server up and running on PORT ${PORT}`));
