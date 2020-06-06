3e586aac08318c8c285e4e018d8b10145591b27a
Siddharth Singh <siddharths51700@gmail.com>
2020-06-06 12:47:31 +0530

    lets c2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 const app = express();
